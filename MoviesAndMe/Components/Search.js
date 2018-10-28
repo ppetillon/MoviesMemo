41b71218553851a5a0c7b300c2657b99782b6c06
Pierre PETILLON <pierre.pet@hotmail.fr>
2018-10-28 21:32:53 +0100

    Flexbox

--- MoviesAndMe/Components/Search.js
+++ MoviesAndMe/Components/Search.js
@@ -6,8 +6,8 @@ import { StyleSheet, View, TextInput, Button } from 'react-native'
 class Search extends React.Component {
   render() {
     return (
-      <View style={{ marginTop: 20 }}>
-        <TextInput style={styles.textinput} placeholder='Titre du film'/>
+      <View style={styles.main_container}>
+        <TextInput style={[styles.textinput, { marginBottom: 10 }]} placeholder='Titre du film'/>
         <Button style={{ height: 50 }} title='Rechercher' onPress={() => {}}/>
       </View>
     )
@@ -22,7 +22,11 @@ const styles = StyleSheet.create({
     borderColor: '#000000',
     borderWidth: 1,
     paddingLeft: 5
-  }
+  },
+  main_container: {
+    flex: 1,
+    marginTop: 20
+  },
 })
 
 export default Search
